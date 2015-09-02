0aec08b62a5bac57a6ee95c83dc6d5368d8c036d
shun.zheng <shun.zheng@corp.elong.com>
2015-09-02 12:44:15 +0800

    worker apply complete

--- entry/js/src/workerapply.js
+++ entry/js/src/workerapply.js
@@ -1,3 +1,23 @@
+// workerapply.js
 define(['../lib/zepto'], function(zepto) {
-	console.log("aaaaaaa");
+	var workerApply = {
+		DOM_DROP: $('#wk-apply-type'),
+		DOM_DROPLIST: $('#wk-apply-formset .form-set-sub'),
+		DOM_DROPITEM: $('#wk-apply-formset .form-set-sub .item-sub'),
+
+		init: function() {
+			var THIS = this;
+			THIS.DOM_DROP.off("click").on("click", function() {
+				THIS.DOM_DROPLIST.toggleClass("on");
+			});
+			THIS.DOM_DROPITEM.off("click").on("click", function() {
+				var index = $(this).index();
+				var txt = $(this).text();
+				console.log(txt);
+				THIS.DOM_DROP.find('input').val(txt);
+				THIS.DOM_DROPLIST.removeClass("on");
+			});
+		}
+	};
+	workerApply.init();
 });

cccfce82d2db69640aa0c893879d9866d32e3017
shun.zheng <451833525@qq.com>
2015-09-13 15:55:01 +0800

    detail199

--- dist/build.js
+++ dist/build.js
@@ -4207,8 +4207,31 @@ define('entry/js/src/quoteres',['../core/core', './component/slideOptions'], fun
 	});
 	
 });
+define('entry/js/src/detail199.js',['../core/core'], function(core) {
+	core.onrender("detail-199", function(dom) {
+		/*-webkit-animation: .5s detail-price-199;*/
+		var Tools = core.Tools;
+
+		var detail199 = {
+			DOM_single: $('.single-page', dom),
+			DOM_price: $('#detail-199-price', dom),
+
+			init: function() {
+				var _this = this;
+				this.DOM_single
+				var price_h = parseFloat(Tools.getCurrentStyle(_this.DOM_price[0], "width")) * 0.4;
+				var single_h = parseFloat(Tools.getCurrentStyle(_this.DOM_single[0], "width")) / 0.5633;
+				_this.DOM_price.css("height", price_h + "px");
+				_this.DOM_single.css("height", single_h + "px");
+				_this.DOM_price.addClass("price-animation");
+			}
+		};
+
+		detail199.init();
+	});
+});
 // main.js
-require(['entry/js/src/workerapply','entry/js/src/quote', 'entry/js/src/quoteres'], function() {
+require(['entry/js/src/workerapply','entry/js/src/quote', 'entry/js/src/quoteres', 'entry/js/src/detail199.js'], function() {
 	
 });
 define("entry/js/main", function(){});
